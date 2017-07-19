f27d2146a28ee80882408fbefe87bdf26a71a90f
Kosta <kest.bodnya@gmail.com>
2017-07-19 21:22:54 +0200

    countdown form testing

--- tests/components/countdownform.test.js
+++ tests/components/countdownform.test.js
@@ -1,10 +1,50 @@
 import React from 'react'
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 
 import CountdownForm from '../../src/components/CountdownForm';
 
 describe('countdown form test suite', () => {
     it('simply exists', () => {
         expect(shallow(<CountdownForm />)).toBeTruthy();
+    });
+
+    describe('onSetCountdown triggering', () => {
+        it('should NOT call onSetCountdown', () => {
+            const onSetCountdown = jest.fn();
+            const countdownForm =
+                mount(<CountdownForm onSetCountdown={onSetCountdown} />);
+
+            countdownForm.find('form').simulate('submit');
+
+            expect(onSetCountdown).not.toBeCalled();
+        });
+
+        it('should NOT call onSetCountdown with string input', () => {
+            const onSetCountdown = jest.fn();
+            const countdownForm =
+                mount(<CountdownForm onSetCountdown={onSetCountdown} />);
+
+            const input = countdownForm.ref('seconds');
+            input.node.value = 'asd';
+            input.simulate('change', input);
+
+            countdownForm.find('form').simulate('submit');
+
+            expect(onSetCountdown).not.toBeCalled();
+        });
+
+        it('should call onSetCountdown with digits input', () => {
+            const onSetCountdown = jest.fn();
+            const countdownForm =
+                mount(<CountdownForm onSetCountdown={onSetCountdown} />);
+
+            const input = countdownForm.ref('seconds');
+            input.node.value = '123';
+            input.simulate('change', input);
+
+            countdownForm.find('form').simulate('submit');
+
+            expect(onSetCountdown).toBeCalled();
+        });
     });
 });
