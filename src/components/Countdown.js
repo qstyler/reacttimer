568f40023d6c6844e038c1b977c194d3d571ac4d
Kosta <kest.bodnya@gmail.com>
2017-07-19 21:39:05 +0200

    countdown timer actually counts down

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import Clock from './Clock';
 import CountdownForm from './CountdownForm';
 
+const STARTED = 'started';
+const STOPPED = 'stopped';
+
 class Countdown extends Component {
 
 
@@ -9,16 +12,46 @@ class Countdown extends Component {
         super(props, context);
 
         this.handleSetCountdown = this.handleSetCountdown.bind(this);
+        this.componentDidUpdate = this.componentDidUpdate.bind(this);
+        this.startTimer = this.startTimer.bind(this);
 
         this.state = {
             count: undefined,
+            countdownStatus: STOPPED,
         };
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        const status = this.state.countdownStatus;
+        if (status !== prevState.countdownStatus) {
+            switch (status) {
+                case STARTED:
+                    this.startTimer();
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 
     handleSetCountdown(seconds) {
         this.setState({
             count: seconds,
+            countdownStatus: STARTED,
         })
+    }
+
+    startTimer() {
+        this.timer = setInterval(() => {
+            const newCount = this.state.count - 1;
+            if (newCount >= 0) {
+                this.setState({
+                    count: newCount,
+                });
+            } else {
+                clearInterval(this.timer)
+            }
+        }, 1000);
     }
 
     render() {
