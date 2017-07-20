6d343c3825bc715d382b1552cd5d5ddd56d6a2c6
Kosta <kest.bodnya@gmail.com>
2017-07-20 23:08:19 +0200

    timer test

---
+++ tests/components/timer.test.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { mount, shallow } from 'enzyme';
+import 'jest-enzyme';
+
+import Timer from '../../src/components/Timer';
+import { Status } from '../../src/Status';
+
+describe('Timer test suite', () => {
+    it('Timer should be true', () => {
+        expect(shallow(<Timer />)).toBeTruthy();
+    });
+
+    describe('timing functions', () => {
+
+        it('starts the timer', (done) => {
+            const timer = shallow(<Timer />);
+            timer.instance().handleStatusChange(Status.STARTED);
+
+            expect(timer).toHaveState('count', 0);
+            expect(timer).toHaveState('status', Status.STARTED);
+
+            setTimeout(() => {
+                expect(timer).toHaveState('count', 1);
+                done();
+            }, 1001);
+        });
+
+        it('pauses the timer', (done) => {
+            const timer = mount(<Timer />);
+            timer.instance().handleStatusChange(Status.STARTED);
+
+            timer.find('button.secondary').simulate('click');
+            expect(timer).toHaveState('status', Status.PAUSED);
+
+            setTimeout(() => {
+                expect(timer).toHaveState('count', 0);
+                done();
+            }, 1001);
+        });
+
+    });
+
+});
